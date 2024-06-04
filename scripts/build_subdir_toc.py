2217b8780898ebbd57592136e11037ddd23c5a54
Lacey Henschel <laceynwilliams@gmail.com>
2024-06-03 21:06:08 -0700

    :sparkles: Add script to generate subdirectory table of contents

---
+++ scripts/build_subdir_toc.py
@@ -0,0 +1,44 @@
+import os
+
+import click
+
+
+@click.command()
+@click.argument("directory", type=click.Path(exists=True))
+def generate_readme_links(directory):
+    """
+    Generate a README file with clickable links to all files in the specified directory.
+
+    DIRECTORY: The path to the directory containing files.
+    """
+    readme_file = os.path.join(directory, "README.md")
+
+    with open(readme_file, "w") as f:
+        # Use first h1 as the header
+        header = os.path.basename(directory)
+        f.write(f"# {header}\n\n")
+
+        for root, dirs, files in os.walk(directory):
+            contents = []
+            for file in files:
+                title = None
+                # Read the file so I can get the header, which contains the title.
+                with open(os.path.join(root, file), "r") as file_obj:
+                    title = file_obj.readline().replace("#", "").strip()
+
+                # Skip files that don't have a title.
+                if not title:
+                    continue
+
+                file_path = os.path.join(root, file)
+                relative_path = os.path.relpath(file_path, directory)
+                contents.append(f"- [{title}]({relative_path})")
+
+            sorted_contents = sorted(contents)
+            f.write("\n".join(sorted_contents))
+
+    click.echo(f"README file generated at {readme_file}")
+
+
+if __name__ == "__main__":
+    generate_readme_links()
